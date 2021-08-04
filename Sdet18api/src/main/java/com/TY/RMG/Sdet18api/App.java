b9e4e634bab6b1619bcb9806aa7ed681697bcc54
padma9900 <padmapatelkh@gmail.com>
2021-08-04 13:49:18 +0530

    api framework created

---
+++ Sdet18api/src/main/java/com/TY/RMG/Sdet18api/App.java
@@ -0,0 +1,7 @@
+package com.TY.RMG.Sdet18api;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ Sdet18api/src/test/java/com/TY/RMG/Sdet18api/ApiFramework.java
@@ -0,0 +1,12 @@
+package com.TY.RMG.Sdet18api;
+
+import org.testng.annotations.Test;
+
+public class ApiFramework {
+
+	
+	@Test
+	public void apiFramework(){
+		System.out.println("api framework");
+	}
+}
